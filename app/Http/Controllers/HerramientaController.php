0d8004295fecfd6649d0128c22ebd4a955f8255d
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2021-03-08 14:56:35 -0400

    Antes de arduino

--- app/Http/Controllers/HerramientaController.php
+++ app/Http/Controllers/HerramientaController.php
@@ -450,7 +450,7 @@ class HerramientaController extends Controller
     public function anularBaja($id)
     {
         $baja  = BajaHerramienta::findOrFail($id);
-            $herramienta = Herramienta::findOrFail($baja->herramienta_id);
+            $herramienta = Herramienta::withTrashed()->findOrFail($baja->herramienta_id);
             $herramienta->cantidad_taller =
                 $herramienta->cantidad_taller + $baja->cantidad;
             $herramienta->cantidad_total =
